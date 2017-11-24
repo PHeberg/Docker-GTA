cc41dc481798fc6f5fc8845cd044dda3332e8061
PHMarc <moreau.marc54@gmail.com>
2017-11-24 01:14:42 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,33 +11,7 @@ echo "#   Le script verifie si mono est     #"
 echo "#              installer              #"
 echo "#######################################"
 if ! [ -x "$(command -v mono)" ]; then
-echo 'Error: mono is not installed.' >&2
-echo "#######################################"
-echo "#   Télechargement de mono            #"
-echo "#######################################"
-wget https://download.mono-project.com/sources/mono/mono-4.8.1.0.tar.bz2
-echo "#######################################"
-echo "#   Décompression de mono.            #"
-echo "#######################################"
-tar xvjf mono-4.8.1.0.tar.bz2
-echo "#######################################"
-echo "#   Configuration de mono             #"
-echo "#######################################"
-cd mono-4.8.1
-sh ./configure
-echo "#######################################"
-echo "#   Compilation de mono               #"
-echo "#   Temps approximatif 20 minutes     #"
-echo "#######################################"
-make
-make install
-echo "#######################################"
-echo "#   Mise a jours des ca-certificates  #"
-echo "#               de mono.              #"
-echo "#######################################"
-cert-sync /etc/ssl/certs/ca-certificates.crt
-cert-sync --user /etc/ssl/certs/ca-certificates.crt
-cd ..
+apt-get install mono-compltete
 
 echo "#######################################"
 echo "#  Récuperation du serveur GTA depuis #"
@@ -45,8 +19,6 @@ echo "#   notre CDN                         #"
 echo "#######################################"
 wget https://cdn.privateheberg.com/GTA/serveur-gta.zip && unzip serveur-gta.zip && rm serveur-gta.zip && chmod 777 -R serveur-gta && cd serveur-gta
 chmod 777 PHgta.sh
-else
-  echo 'mono is installed'
 fi
 
 echo "#######################################"
